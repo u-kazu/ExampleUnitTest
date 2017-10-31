eae3cf3ac4d0cb09ca8803d69fed4bc8d3d226c1
kazuma Uehara <e175740@ie.u-ryukyu.ac.jp>
2017-10-31 16:10:33 +0900

    step1 commit

--- src/main/jp/ac/uryukyu/ie/e175740/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e175740/Enemy.java
@@ -46,10 +46,12 @@ public class Enemy {
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
      * @param hero 攻撃対象
      */
-    public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+    public void attack(Hero hero) {
+        if(!isDead()){
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
+        }else{ }
     }
 
     /**
